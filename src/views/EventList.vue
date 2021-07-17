472faf27c0de6bf5150f60cd5391e8271ea552c7
Yuriy Titov <yuranus@gmail.com>
2021-07-17 20:32:42 +0300

    complete L3-start

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -3,30 +3,72 @@
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
+
+  <div class="pagination">
+    <router-link
+      id="page-prev"
+      :to="{ name: 'EventList', query: { page: page - 1 } }"
+      rel="prev"
+      v-if="page != 1"
+      >&#60; Prev</router-link
+    >
+
+    <ul v-for="page in totalPages" :key="page">
+      <li>
+        <router-link :to="{ name: 'EventList', query: { page: page } }">{{
+          page
+        }}</router-link>
+      </li>
+    </ul>
+
+    <router-link
+      id="page-next"
+      :to="{ name: 'EventList', query: { page: page + 1 } }"
+      rel="next"
+      v-if="hasNextPage"
+      >Next &#62;</router-link
+    >
+  </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
+import { watchEffect } from 'vue'
 
 export default {
   name: 'EventList',
+  props: ['page'],
   components: {
     EventCard
   },
   data() {
     return {
-      events: null
+      events: null,
+      totalEvents: 0
     }
   },
   created() {
-    EventService.getEvents()
-      .then(response => {
-        this.events = response.data
-      })
-      .catch(error => {
-        console.log(error)
-      })
+    watchEffect(() => {
+      this.events = null
+      EventService.getEvents(2, this.page)
+        .then(response => {
+          this.events = response.data
+          this.totalEvents = response.headers['x-total-count']
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    })
+  },
+  computed: {
+    hasNextPage() {
+      let totalPages = Math.ceil(this.totalEvents / 2)
+      return this.page < totalPages
+    },
+    totalPages() {
+      return Math.ceil(this.totalEvents / 2)
+    }
   }
 }
 </script>
@@ -36,5 +78,36 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.pagination {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  /* width: 290px; */
+}
+
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+.pagination ul {
+  list-style: none;
+}
+.pagination li {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  margin-right: 1%;
+  text-align: right;
+}
+
+#page-next {
+  margin-left: 1%;
+  text-align: left;
 }
 </style>
