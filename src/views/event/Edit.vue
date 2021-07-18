92c8989307f104eca3a87f81b62f793854fe06b3
Yuriy Titov <yuranus@gmail.com>
2021-07-18 08:15:37 +0300

    complete L9-start

--- src/views/event/Edit.vue
+++ src/views/event/Edit.vue
@@ -3,7 +3,29 @@
 </template>
 
 <script>
-export default {
-  props: ['event']
-}
+// export default {
+//   props: ['event'],
+//   data: function() {
+//     return {
+//       unsavedChanges: true
+//     }
+//   },
+//   // beforeRouteLeave() {
+//   //   if (this.unsavedChanges) {
+//   //     const answer = window.confirm(
+//   //       'Do you really want leave? You have unsaved changes.'
+//   //     )
+//   //     !answer ? false : true
+//   //   }
+//   // }
+//   // beforeRouteLeave(routeTo, routeFrom, next) {
+//   //   if (this.unsavedChanges) {
+//   //     const answer = window.confirm(
+//   //       'Do you really want leave? You have unsaved changes.'
+//   //     )
+//   //     answer ? next() : next(false)
+//   //   } else next()
+//   // }
+// }
+//
 </script>
