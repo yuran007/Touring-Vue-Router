8a010bb6ce05ca2b9ee9b07f83c8821575bf1d8b
Yuriy Titov <yuranus@gmail.com>
2021-07-18 04:53:10 +0300

    complete L5-start

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ const routes = [
     props: route => ({ page: parseInt(route.query.page) || 1 })
   },
   {
-    path: '/event/:id',
+    path: '/events/:id',
     name: 'EventLayout',
     props: true,
     component: EventLayout,
@@ -37,9 +37,40 @@ const routes = [
     ]
   },
   {
-    path: '/about',
+    // using (.*) include / in match(by deffault it doesn't)
+    path: '/event/:afterEvent(.*)',
+    redirect: to => {
+      return { path: '/events/' + to.params.afterEvent }
+    }
+
+    // path: '/event/:id',
+    // redirect: () => {
+    //   return { name: 'EventDetails' }
+    // },
+    // children: [
+    //   {
+    //     path: 'register',
+    //     redirect: () => ({ name: 'EventRegister' })
+    //   },
+    //   {
+    //     path: 'edit',
+    //     redirect: () => ({ name: 'EventEdit' })
+    //   }
+    // ]
+  },
+  //How to change /about to /about-us?
+  {
+    path: '/about-us',
+    //path: '/about',
     name: 'About',
     component: About
+    // 2 variant add alias
+    // alias: '/about'
+  },
+  // 1 variant add redirect
+  {
+    path: '/about',
+    redirect: { name: 'About' }
   }
 ]
 
